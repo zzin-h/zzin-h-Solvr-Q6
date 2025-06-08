2552180bdd8b0591c601686d711200e63880344c
jelly12paw <jelly12paw@gmail.com>
2025-06-08 21:14:30 +0900

    fix: 라우터 중복 제거

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import SleepEntryListPage from './pages/SleepEntryListPage'
 import NewSleepEntryPage from './pages/NewSleepEntryPage'
@@ -7,19 +7,17 @@ import EditSleepEntryPage from './pages/EditSleepEntryPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          {/* 루트 경로를 수면 기록 목록으로 리다이렉트 */}
-          <Route path="/" element={<Navigate to="/sleep-entries" replace />} />
+    <Layout>
+      <Routes>
+        {/* 루트 경로를 수면 기록 목록으로 리다이렉트 */}
+        <Route path="/" element={<Navigate to="/sleep-entries" replace />} />
 
-          {/* 수면 기록 관련 라우트 */}
-          <Route path="/sleep-entries" element={<SleepEntryListPage />} />
-          <Route path="/sleep-entries/new" element={<NewSleepEntryPage />} />
-          <Route path="/sleep-entries/:id/edit" element={<EditSleepEntryPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+        {/* 수면 기록 관련 라우트 */}
+        <Route path="/sleep-entries" element={<SleepEntryListPage />} />
+        <Route path="/sleep-entries/new" element={<NewSleepEntryPage />} />
+        <Route path="/sleep-entries/:id/edit" element={<EditSleepEntryPage />} />
+      </Routes>
+    </Layout>
   )
 }
 
